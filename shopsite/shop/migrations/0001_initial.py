a47a4952acff08ea6194481108a9c96a7ac438d4
Mikhail Hatsilau <mikhail_hatsilau@epam.com>
2016-01-12 13:37:53 +0300

    Unnecessary migrations have been removed

--- shopsite/shop/migrations/0001_initial.py
+++ shopsite/shop/migrations/0001_initial.py
@@ -25,7 +25,7 @@ class Migration(migrations.Migration):
             name='Order',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('date', models.DateField()),
+                ('date', models.DateTimeField()),
             ],
         ),
         migrations.CreateModel(
@@ -36,8 +36,8 @@ class Migration(migrations.Migration):
                 ('discription', models.CharField(max_length=600)),
                 ('price', models.DecimalField(default=0, max_digits=8, decimal_places=2)),
                 ('image', models.ImageField(default=b'static/images/default_product.png', upload_to=b'static/images')),
-                ('from_date', models.DateTimeField(default=datetime.datetime(2016, 1, 6, 10, 53, 58, 633275, tzinfo=utc))),
-                ('to_date', models.DateTimeField(default=datetime.datetime(2016, 1, 13, 10, 53, 58, 633313, tzinfo=utc))),
+                ('from_date', models.DateTimeField(default=datetime.datetime(2016, 1, 12, 10, 32, 43, 521316, tzinfo=utc))),
+                ('to_date', models.DateTimeField(default=datetime.datetime(2016, 1, 19, 10, 32, 43, 521352, tzinfo=utc))),
                 ('category', models.ForeignKey(to='shop.Category')),
                 ('seller', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
